bfaf6bd9291e90561c858649138184186f2a65f3
Miguel Rocha <miguelantoniooliveirarocha@gmail.com>
2018-11-15 14:38:11 +0000

    BALL

--- js/Game.js
+++ js/Game.js
@@ -3,7 +3,9 @@ class Game{
     constructor(){
 
         this.table = new Table();
+        this.eightballpool = new EightBallPool();
         scene.add(this.table);
+        scene.add(this.eightballpool);
 
     }
 
