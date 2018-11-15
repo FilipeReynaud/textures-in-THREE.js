990d2a812a6a947b819a3571bb5ae68a664d0ee1
Artur Fortunato <artur.fortunato1@gmail.com>
2018-11-15 19:38:09 +0000

    Texto incluido, precisa de ser redimensionado

--- js/Game.js
+++ js/Game.js
@@ -1,19 +1,21 @@
 const MAX_VELOCITY = 15;
-
+var object;
 class Game{
 
     constructor(){
         this.table = new Table();
-
         this.cube = new RubikCube();
+        this.message = new Text();
+        this.eightBallPool = new EightBallPool();
+
         scene.add(this.table);
         scene.add(this.cube);
-        this.translactionAngle = 0;
-        this.translactionVelocity = 0;
-        this.eightBallPool = new EightBallPool();
         scene.add(this.table);
         scene.add(this.eightBallPool);
 
+        this.translactionAngle = 0;
+        this.translactionVelocity = 0;
+        object = this;
     }
 
     moveBall(time, accelaration) {
@@ -21,7 +23,7 @@ class Game{
             this.translactionVelocity = 0;
             return true;
         }
-        else if(this.translactionVelocity >= MAX_VELOCITY)
+        else if(this.translactionVelocity >= MAX_VELOCITY && accelaration != -1)
             this.translactionVelocity = MAX_VELOCITY;
         else
             this.translactionVelocity += time * accelaration;
@@ -35,13 +37,20 @@ class Game{
     }
 
     rotateBall(){
-    	var quaternion = new THREE.Quaternion();
-    	var angle = this.translactionVelocity;
+        var quaternion = new THREE.Quaternion();
+        var angle = this.translactionVelocity;
 
 
-    	quaternion.setFromAxisAngle(new THREE.Vector3(this.eightBallPool.position.z - this.prevZ, 0, -(this.eightBallPool.position.x - this.prevX)).normalize(), angle/10);
-    	this.eightBallPool.children[0].applyQuaternion(quaternion);
-   	}
+        quaternion.setFromAxisAngle(new THREE.Vector3(this.eightBallPool.position.z - this.prevZ, 0, -(this.eightBallPool.position.x - this.prevX)).normalize(), angle/10);
+        this.eightBallPool.children[0].applyQuaternion(quaternion);
+    }
 
+    pause() {
+        scene.add(this.message);
+        this.paused = true;
+    }
 
+    unpause() {
+        scene.children.splice(scene.children.indexOf(this.message), 1);
+    }
 }
