3b59a4f81deac4f30c46b748b5f7800140078fdf
Filipe Reynaud <reynaudfilipe@gmail.com>
2018-11-16 14:22:38 +0000

    vou recebr

--- js/Table.js
+++ js/Table.js
@@ -2,14 +2,15 @@ class Table extends Objeto{
 
     constructor(){
         super();
-        var texture = new THREE.TextureLoader().load( "../textures/chess.png");
 
-        var material = new THREE.MeshPhongMaterial({color: 0x696969, shininess: 5, bumpMap: texture, bumpScale: 0.20});
+        var texture = new THREE.TextureLoader().load( "../textures/chess.png");
 
         texture.wrapS = THREE.RepeatWrapping;
         texture.wrapT = THREE.RepeatWrapping;
         texture.repeat.set(4, 4);
-        material.map = texture;
+        texture.anisotropy = renderer.capabilities.getMaxAnisotropy();
+
+        var material = new THREE.MeshStandardMaterial({ map: texture, roughness: 1, color: 0x696969, shininess: 500, bumpMap: texture, bumpScale: 0.20});
 
         this.addElement(0, 0, 0, new THREE.BoxGeometry(10, 0.1, 10, 10, 10, 10), material);
 
