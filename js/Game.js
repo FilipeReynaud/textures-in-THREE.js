5f3dfd03f5aff455932cc1800885a03ac65b0597
Artur Fortunato <artur.fortunato1@gmail.com>
2018-11-18 18:13:40 +0000

    Código limpo e condicoes adicionadas para evitar o calculo de luz com a cena em pause

--- js/Game.js
+++ js/Game.js
@@ -6,13 +6,11 @@ class Game{
     constructor(){
         this.table = new Table();
         this.cube = new RubikCube();
-        this.message = new Text();
         this.eightBallPool = new EightBallPool();
         this.HUD = new HUD();
 
         scene.add(this.table);
         scene.add(this.cube);
-        scene.add(this.table);
         scene.add(this.eightBallPool);
 
         pauseScene.add(this.HUD);
@@ -44,22 +42,12 @@ class Game{
         var quaternion = new THREE.Quaternion();
         var angle = this.translactionVelocity;
 
-
         quaternion.setFromAxisAngle(new THREE.Vector3(this.eightBallPool.position.z - this.prevZ, 0, -(this.eightBallPool.position.x - this.prevX)).normalize(), angle/10);
         this.eightBallPool.children[0].applyQuaternion(quaternion);
     }
 
-    refreshTextPosition() {
-        this.message.position.copy( orthographicCamera.position );
-        this.message.rotation.copy( orthographicCamera.rotation );
-        this.message.updateMatrix();
-        this.message.translateZ( - 10 );
-        this.message.translateX( - 3 );
-    }
-
     pause() {
         this.paused = true;
-        this.refreshTextPosition();
     }
 
     unpause() {
